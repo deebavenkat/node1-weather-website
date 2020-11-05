fc4e271ef62456d485bda1c44b835df891daa6a3
deebavenkat <deeba.venkatraman@pirean.com>
2020-11-05 21:57:07 +0000

    To include new data humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,12 +4,13 @@ const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=d907f37148885a14abfffc9edd7d4346&query=' + latitude + ',' + longitude + '&units=m'
 
     request({url, json: true}, (error, { body }) => {
+        console.log(body)
         if (error) {
             callback('Unable to connect to weather services!', undefined)
         } else if (body.error) {
             callback('Unable to find the location!',undefined)
         } else {
-           callback(undefined, body.current.weather_descriptions[0]+'. It is currently '+ body.current.temperature + ' degrees out. It feels like '+ body.current.feelslike + ' degrees out.') 
+           callback(undefined, body.current.weather_descriptions[0]+'. It is currently '+ body.current.temperature + ' degrees out. It feels like '+ body.current.feelslike + ' degrees out. Humidity is ' + body.current.humidity + '%.') 
         }
     })
 }
